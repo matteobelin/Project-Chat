a0e8c9b261defcc74a3acb9dfba4346b2fa6cff2
matteobelin <matteobelin85@gmail.com>
2023-10-11 11:42:03 +0200

    Envoyer des messages et afficher

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'views/index.html'));
+});
+
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg)
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
